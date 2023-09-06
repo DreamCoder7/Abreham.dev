fb1c885ec26620511d167b7324b366be1893b7a8
Abraham <nigussieabreham80@gmail.com>
2023-09-06 18:49:49 +0300

    Added utils for reusable functionality and constants for exporting contents

---
+++ src/app/_utils/constants/index.ts
@@ -0,0 +1,7 @@
+import { IntroProps } from "../../_types";
+
+export const introContent: IntroProps = {
+  title: "Full-Stack Software Developer",
+  text: "Resolving design problems, building smart user interfaces and useful interactions, developing rich web applications and seamless web experiences.",
+  btnText: "About me",
+};
