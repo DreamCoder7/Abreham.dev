924295109af65e648c9af26891254af365d0c58d
Abraham <nigussieabreham80@gmail.com>
2023-09-07 09:18:16 +0300

    modify layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import './_styles/global.scss';
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
