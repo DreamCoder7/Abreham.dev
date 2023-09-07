45e3a3791678d0866aaa35780334fd5d7eae7ed8
Abraham <nigussieabreham80@gmail.com>
2023-09-07 09:16:07 +0300

    modifying Intro component

--- src/app/_components/Intro/Intro.tsx
+++ src/app/_components/Intro/Intro.tsx
@@ -1,33 +1,38 @@
 import Link from "next/link";
 
 import { introContent } from "@/app/_utils/constants";
-import Logo from "../Logo/Logo";
+import LottieAnimation from "../../_assets/lottie/portfolio.json";
+import DisplayLottie from "../DisplayLottie/DisplayLottie";
+import classes from "./Intro.module.scss";
+import Nav from "../Nav/Nav";
+import Scroll from "../Scroll/Scroll";
+import { IoIosArrowForward } from "react-icons/io";
+import { FaGithubAlt } from "react-icons/fa";
 
 function Intro() {
   const { btnText, text, title } = introContent;
 
   return (
-    <header>
-      <nav>
-        <Logo isBgDark />
-        <ul>
-          <li>
-            <Link href="/abreham.blog.com">Blog</Link>
-          </li>
-          <li>
-            <Link href="/abreham.blog.com">Resume</Link>
-          </li>
-          <li>
-            <button>Contact</button>
-          </li>
-        </ul>
-      </nav>
-      <div>
-        <h1>{title}</h1>
-        <p>{text}</p>
-        <button>{btnText}</button>
+    <header className={classes.Header}>
+      <Nav />
+      <div className={classes.Content}>
+        <h1 className={classes.Title}>{title}</h1>
+        <p className={classes.Text}>{text}</p>
+        <button className={classes.AboutBtn}>
+          {btnText}
+          <IoIosArrowForward className={classes.Icon} />
+        </button>
       </div>
-      <div>{/* Lottie files goes here */}</div>
+      <div className={classes.ImgCon}>
+        <DisplayLottie animationData={LottieAnimation} />
+      </div>
+      <Link
+        href="https://github.com/DreamCoder7"
+        className={classes.GithubLink}
+      >
+        <FaGithubAlt className={classes.GitHubIcon} />
+      </Link>
+      <Scroll />
     </header>
   );
 }
