45c43f0aee64e13bbd66ec6200d9490a7895ca21
Abraham <nigussieabreham80@gmail.com>
2023-09-09 13:26:31 +0300

    Added Logo

--- src/app/_components/Logo/Logo.tsx
+++ src/app/_components/Logo/Logo.tsx
@@ -1,16 +1,43 @@
+"use client";
+import { useContext } from "react";
 import Image from "next/image";
+import { PiMoonFill } from "react-icons/pi";
+import { BsFillSunFill } from "react-icons/bs";
 
-type LogoProps = {
-  isBgDark: boolean;
-};
+import classes from "./Logo.module.scss";
+import { ModeContext } from "@/app/_context/modeContext";
 
-function Logo(props: LogoProps) {
-  const { isBgDark } = props;
+function Logo() {
+  const { darkModeHandler, lightModeHandler, mode } = useContext(ModeContext);
 
-  return isBgDark ? (
-    <Image src="/logo-white.svg" alt="Logo DreamCoder" width={50} height={50} />
-  ) : (
-    <Image src="/logo-dark.svg" alt="Logo DreamCoder" width={50} height={50} />
+  return (
+    <div className={classes.Logo}>
+      {mode === "dark" ? (
+        <Image
+          src="/logo-white.svg"
+          alt="Logo DreamCoder"
+          width={50}
+          height={50}
+        />
+      ) : (
+        <Image
+          src="/logo-dark.svg"
+          alt="Logo DreamCoder"
+          width={50}
+          height={50}
+        />
+      )}
+      <div>
+        {mode === "dark" ? (
+          <BsFillSunFill
+            className={classes.SunIcon}
+            onClick={lightModeHandler}
+          />
+        ) : (
+          <PiMoonFill className={classes.MoonIcon} onClick={darkModeHandler} />
+        )}
+      </div>
+    </div>
   );
 }
 
